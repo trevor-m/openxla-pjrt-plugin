cfc9809dc8d393943c0112a49e6a374e44d4d6ed
Okwan Kwon <okkwon@gmail.com>
2023-05-10 14:56:48 -0700

    add keyvaluestore interface

---
+++ iree/integrations/pjrt/common/keyvaluestore.h
@@ -0,0 +1,25 @@
+// Copyright 2023 The IREE Authors
+//
+// Licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef IREE_PJRT_PLUGIN_PJRT_KEYVALUESTORE_H_
+#define IREE_PJRT_PLUGIN_PJRT_KEYVALUESTORE_H_
+
+#include <string>
+#include <vector>
+
+namespace iree::pjrt {
+
+class KeyValueStore {
+ public:
+  virtual ~KeyValueStore();
+  virtual bool set(const std::string& key,
+                   const std::vector<uint8_t>& value) = 0;
+  virtual bool get(const std::string& key, std::vector<uint8_t>& value) = 0;
+};
+
+}  // namespace iree::pjrt
+
+#endif  // IREE_PJRT_PLUGIN_PJRT_KEYVALUESTORE_H_
