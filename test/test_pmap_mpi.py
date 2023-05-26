310e1d314bd01c282af3ee51a00c179784bccb92
Okwan Kwon <okkwon@gmail.com>
2023-05-26 14:33:04 -0700

    [mpi test] print out global & local device counts

--- test/test_pmap_mpi.py
+++ test/test_pmap_mpi.py
@@ -7,6 +7,12 @@ import sys
 
 def test_pmap_mpi():
     jax.distributed.initialize()
+
+    global_device_count = jax.device_count()
+    local_device_count = jax.local_device_count()
+    print(f"device_count = {global_device_count}")
+    print(f"local_device_count = {local_device_count}")
+
     xs = jax.numpy.ones(jax.local_device_count())
     sum = jax.pmap(lambda x: jax.lax.psum(x, 'i'), axis_name='i')(xs)
 
